c12ebed30540e460a49868ec2ba92bf64b394877
Tomas Harkema <info@tomasharkema.nl>
2014-12-06 20:27:50 +0100

    some setters and getters

--- test/device-test.js
+++ test/device-test.js
@@ -5,7 +5,8 @@ var Device = require('../lib/device').Device;
 
 beforeEach(function(done){
     return Device.remove({}, function() {
-        var d = new Device({name:"testDevice", type:"test"});
+        var d = new Device();
+        d.name = "test";
         return d.save(function(){
             done();
         });
@@ -15,7 +16,8 @@ beforeEach(function(done){
 describe('Devices', function(){
     describe('#addDevice', function(){
         it('should have 2 devices after add 1 device', function(done){
-            var d = new Device({name:"testDevice", type:"test"});
+            var d = new Device();
+            d.name = "test";
             return d.save(function(){
                 return Device.find({}, function(err, res){
                     if (res.length === 2) {
@@ -24,6 +26,28 @@ describe('Devices', function(){
                         throw new Error();
                     }
                 })
+            });
+        })
+
+        it('should call my handler when I add a device', function(done){
+            var d = new Device();
+            d.name = "test";
+            Device.once(function(){
+                done();
+            });
+            d.save();
+        })
+    })
+})
+
+
+describe('Device', function(){
+    describe('#turnOff', function(){
+        it('should turn off when called', function(done){
+            Device.findOne({}, function(err, d){
+                d.turnOff(function(d){
+                    done();
+                });
             });
         })
     })
