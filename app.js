d1d2d751183fc3056ad856f4dfa1600810a757b3
Tomas Harkema <info@tomasharkema.nl>
2014-12-06 13:32:20 +0100

    start up with socket io. do a test on heroku

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -58,4 +60,5 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+exports.app = app;
+exports.server = server;
