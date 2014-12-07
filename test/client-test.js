ff7e24b8848e735e376d7179a942335b6b2a0e6c
Tomas Harkema <info@tomasharkema.nl>
2014-12-07 20:41:01 +0100

    simple client/server tests

---
+++ test/client-test.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = "TEST";
+
+var assert = require("assert");
+var server = require('../bin/server');
+var client = require('../bin/client');
+
+
+describe('Client', function(){
+
+})
