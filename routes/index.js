48dea6da20743edd139ebefdeb6e8ddfd871201e
Tomas Harkema <info@tomasharkema.nl>
2014-12-07 17:25:33 +0100

    check env over all endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'HomeAppv2' });
+  res.render('index', { title: 'HomeAppv2', footer: "copyleft tomas harkema 2014. "+process.env.NODE_ENV.toLowerCase() });
 });
 
 module.exports = router;
