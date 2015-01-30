98a8fa34a134eb67fa0cd33c40e6a7a380862c22
Tomas Harkema <info@tomasharkema.nl>
2015-01-30 13:27:18 +0100

    exhausive test

--- test/client-test.js
+++ test/client-test.js
@@ -1,10 +1,10 @@
 process.env.NODE_ENV = "TEST";
 process.env.PORT = 7000;
 var assert = require("assert");
+var request = require("request");
 
-
-describe('Client', function(){
-    describe('server', function(){
+describe('Server', function(){
+    describe('server - socket', function(){
         it('should come up', function(done){
             var server = require('../bin/server');
             var client = require('../bin/client');
@@ -12,6 +12,19 @@ describe('Client', function(){
             setTimeout(function(){
                 done();
             }, 1000);
-        })
+        });
+    });
+    describe('server - frontend', function(){
+        it('should come up', function(done){
+            var server = require('../bin/server');
+
+            request("http://localhost:" + process.env.PORT, function(err, res, body){
+                if (err) {
+                    return done(err);
+                }
+
+                done(assert.equal(res.statusCode, 200, "Should return 200"));
+            });
+        });
     });
 });
